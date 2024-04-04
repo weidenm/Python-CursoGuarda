61ee1e45981b473062c543b200cc8de9b2383376
Weiden <weidencore@gmail.com>
2024-04-03 21:00:05 -0300

    Torneio BD - inclusao de consulta de dados

--- pythonTorneioBD/main.py
+++ pythonTorneioBD/main.py
@@ -10,6 +10,13 @@ cursor.execute('''CREATE TABLE IF NOT  EXISTS heroes
     health INTEGER,
     attack INTEGER)''')
 
+
+cursor.execute('''CREATE TABLE IF NOT  EXISTS dragons
+    (id INTEGER PRIMARY KEY AUTOINCREMENT , 
+    name TEXT,
+    health INTEGER,
+    attack INTEGER)''')
+
 class Character:
     def __init__(self, name, health, attack):
         self.name = name
@@ -24,7 +31,7 @@ class Dragon(Character):
     def __str__(self):
         return f"Dragão {self.name}, Vida: {self.health}, Attack: {self.attack}"
 
-
+#INSERE DADOS DOS HEROIS E DRAGOES
 def insert_hero(hero):
     conn = sqlite3.connect("heroes_dragons.db")
     cursor = conn.cursor()
@@ -37,8 +44,48 @@ def insert_hero(hero):
 def insert_dragon(dragon):
     conn = sqlite3.connect("heroes_dragons.db")
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO heroes (name, health, attack) VALUES (?,?,?)",
+    cursor.execute("INSERT INTO dragons (name, health, attack) VALUES (?,?,?)",
                    (dragon.name, dragon.health, dragon.attack))
     conn.commit()
     conn.close()
+
+#RECUPERA OS DADOS DO PERSONAGEM
+
+def get_heroes():
+    conn = sqlite3.connect("heroes_dragons.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM heroes")
+    heroes_data = cursor.fetchall()
+    conn.close()
+
+    heroes = [Hero(*data[1:]) for data in heroes_data]
+    return heroes
+
+def get_dragons():
+    conn = sqlite3.connect("heroes_dragons.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM dragons")
+    dragons_data = cursor.fetchall()
+    conn.close()
+
+    dragons = [Dragon(*data[1:]) for data in dragons_data]
+    return dragons
+
+#CRIA TORNEIO
+def executa_torneio():
+    heroes = get_heroes()
+    dragons = get_dragons()
+
+    for hero in heroes:
+        for dragon in dragons:
+            if hero.attack > dragon.attack:
+                print(f"{hero.name} venceu {dragon.name}")
+            else
+                print(f"{dragon.name} venceu {hero.name}")
+
+
+
+executa_torneio()
+#drag = Dragon('Drak',100, 50)
+#insert_dragon(drag)
 
